34a179087efb055d3762bd6f908da23a161f4244
Daniel Pineda <danielspineda87@gmail.com>
2021-07-24 13:57:08 -0500

    .

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+menuIcon = document.querySelector(".icon-menu");
+menu = document.querySelector(".menu");
+nav = document.querySelector(".nav");
+let active = false;
+menuIcon.addEventListener("click",()=>{
+    if(!active){
+        menu.style.cssText = "right:0;transition:.5s;";
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-times");
+        active=true;
+    }else{
+        menu.style.cssText = "right:-50rem;transition:.6s;";
+        menuIcon.classList.add("fa-bars");
+        menuIcon.classList.remove("fa-times");
+        active=false;
+    }
+    
+});
